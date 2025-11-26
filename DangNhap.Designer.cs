0cb00bbd70c859ba7a4a30ce598e5266395b683f
Cao Khoa <khoa_dpm235434@student.agu.edu.vn>
2025-11-26 10:17:10 +0700

    Giao dien dang nhap

--- DangNhap.Designer.cs
+++ DangNhap.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DangNhap";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DangNhap));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtTenDangNhap = new TextBox();
+            txtMatKhau = new TextBox();
+            button1 = new Button();
+            btnDangNhap = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(189, 294);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(308, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(200, 41);
+            label1.TabIndex = 1;
+            label1.Text = "ĐĂNG NHẬP";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(223, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(110, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Tên đăng nhập:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(223, 148);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Mật khẩu:";
+            // 
+            // txtTenDangNhap
+            // 
+            txtTenDangNhap.Location = new Point(223, 104);
+            txtTenDangNhap.Name = "txtTenDangNhap";
+            txtTenDangNhap.Size = new Size(353, 27);
+            txtTenDangNhap.TabIndex = 4;
+            // 
+            // txtMatKhau
+            // 
+            txtMatKhau.Location = new Point(223, 185);
+            txtMatKhau.Name = "txtMatKhau";
+            txtMatKhau.Size = new Size(353, 27);
+            txtMatKhau.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Red;
+            button1.Location = new Point(443, 239);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Hủy bỏ";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // btnDangNhap
+            // 
+            btnDangNhap.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDangNhap.ForeColor = Color.Blue;
+            btnDangNhap.Location = new Point(252, 239);
+            btnDangNhap.Name = "btnDangNhap";
+            btnDangNhap.Size = new Size(94, 29);
+            btnDangNhap.TabIndex = 5;
+            btnDangNhap.Text = "Đăng nhập";
+            btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // DangNhap
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(607, 292);
+            Controls.Add(btnDangNhap);
+            Controls.Add(button1);
+            Controls.Add(txtMatKhau);
+            Controls.Add(txtTenDangNhap);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "DangNhap";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Đăng nhập";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtTenDangNhap;
+        private TextBox txtMatKhau;
+        private Button button1;
+        private Button btnDangNhap;
     }
 }
