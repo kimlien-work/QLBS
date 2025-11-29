af7ba23726e66a011d7abb3a4436df87d34982c3
Kim Lien <lethikimlien.work@gmail.com>
2025-11-29 13:51:31 +0700

    form Flash

---
+++ Flash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLBS
+{
+    public partial class Flash : Form
+    {
+        public Flash()
+        {
+            InitializeComponent();
+        }
+
+        private void Flash_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 3000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            this.Close();
+        }
+    }
+}
